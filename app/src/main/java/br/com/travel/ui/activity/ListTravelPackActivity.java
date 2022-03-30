d60ec558a1661a27135eab7485f10d0faeb1efd0
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2022-03-30 17:55:48 -0300

    Created banner of travels

---
+++ app/src/main/java/br/com/travel/ui/activity/ListTravelPackActivity.java
@@ -0,0 +1,16 @@
+package br.com.travel.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.travel.R;
+
+public class ListTravelPackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_travel_pack);
+    }
+}
